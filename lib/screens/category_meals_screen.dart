bca3174213f3f90e543f270b9431b44cd7147b7a
Lenovo <anand9412868527@gmail.com>
2021-03-23 00:41:29 +0530

    initial commit

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:mealsapp/widgets/meal_item.dart';
+import '../dummy-data.dart';
+
+class CategoryMealScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(categoryTitle),
+        ),
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return MealItem(
+                id: categoryMeals[index].id,
+                title: categoryMeals[index].title,
+                affordability: categoryMeals[index].affordability,
+                complexity: categoryMeals[index].complexity,
+                duration: categoryMeals[index].duration,
+                imageUrl: categoryMeals[index].imageUrl);
+          },
+          itemCount: categoryMeals.length,
+        ));
+  }
+}
